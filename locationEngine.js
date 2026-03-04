ee0fa01fa902df7aa7886dd08482b920dbf06688
geceleriesen <etufan@gmail.com>
2026-03-04 21:38:13 +0300

    Update locationEngine.js

--- locationEngine.js
+++ locationEngine.js
@@ -1,1 +1,25 @@
+import {cities} from "./cities.js"
 
+export function detectLocation(text){
+
+const t=text.toLowerCase()
+
+for(const c of cities){
+
+if(t.includes(c[0])){
+
+return{
+
+name:c[0],
+lat:c[1],
+lng:c[2]
+
+}
+
+}
+
+}
+
+return null
+
+}
