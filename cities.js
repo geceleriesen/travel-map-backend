40410577b0d58cc2aa32e38d9aca47e30c5299ec
geceleriesen <etufan@gmail.com>
2026-03-04 19:11:54 +0300

    Create cities.js

---
+++ cities.js
@@ -0,0 +1,9 @@
+export const cities = [
+"istanbul","ankara","izmir","rome","paris","london",
+"cairo","giza","mexico","cancun","tokyo","beijing",
+"shanghai","dubai","bangkok","phuket","bali",
+"san juan","porto rico","puerto rico","barcelona",
+"madrid","athens","new york","los angeles","miami",
+"singapore","kuala lumpur","delhi","mumbai",
+"moscow","berlin","amsterdam","vienna","prague"
+];
