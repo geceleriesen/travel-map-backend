b1105bf8eb8782cfb4c260db70b1e9ddf543e96e
geceleriesen <etufan@gmail.com>
2026-03-04 18:54:29 +0300

    Update and rename locationCache.js to cache.js

---
+++ cache.js
@@ -0,0 +1,43 @@
+import { getAllVideos } from "./youtube.js"
+import { detectLocation } from "./locationAI.js"
+import { geocode } from "./geocode.js"
+
+let cache = []
+
+export function getCache() {
+  return cache
+}
+
+export async function updateCache() {
+
+  const videos = await getAllVideos()
+
+  const results = []
+
+  for (const video of videos) {
+
+    const text = video.title + " " + video.description
+
+    const place = detectLocation(text)
+
+    if (!place) continue
+
+    const geo = await geocode(place)
+
+    if (!geo) continue
+
+    results.push({
+      id: video.id,
+      lat: geo.lat,
+      lng: geo.lng,
+      location: geo.name,
+      title: video.title,
+      shortDescription: video.description.slice(0, 120),
+      thumbnail: video.thumbnail
+    })
+  }
+
+  cache = results
+
+  console.log("Cache updated:", results.length)
+}
