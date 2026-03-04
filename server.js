719f2b748a64900dd28242b8739803d5d2ea143c
geceleriesen <etufan@gmail.com>
2026-03-05 01:29:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 import express from "express"
 import cors from "cors"
 
-import {getVideos} from "./youtube.js"
-
 const app = express()
 
 app.use(cors())
@@ -10,13 +8,70 @@ app.use(cors())
 const PORT = process.env.PORT || 10000
 
 
-app.get("/api/videos", async(req,res)=>{
+const CHANNEL =
+"https://www.youtube.com/@YOUR_CHANNEL/videos"
+
+
+
+function randomLocation(){
+
+return {
+
+lat:20 + (Math.random()*60 - 30),
+
+lng:(Math.random()*120 - 60)
+
+}
+
+}
+
+
+async function getVideos(){
+
+const res = await fetch(CHANNEL)
+
+const html = await res.text()
+
+
+const ids = [...html.matchAll(/"videoId":"(.*?)"/g)]
+
+const unique = [...new Set(ids.map(i=>i[1]))]
+
+
+const videos = unique.map(id=>{
+
+const loc = randomLocation()
+
+return {
+
+id,
+
+title:"YouTube Video",
+
+thumbnail:`https://img.youtube.com/vi/${id}/hqdefault.jpg`,
+
+lat:loc.lat,
+
+lng:loc.lng
+
+}
+
+})
+
+
+return videos
+
+}
+
+
+
+app.get("/api/videos", async (req,res)=>{
 
 try{
 
-const vids = await getVideos()
+const videos = await getVideos()
 
-res.json(vids)
+res.json(videos)
 
 }catch(e){
 
@@ -31,6 +86,6 @@ res.json([])
 
 app.listen(PORT,()=>{
 
-console.log("Travel Map Engine running")
+console.log("Travel backend running")
 
 })
