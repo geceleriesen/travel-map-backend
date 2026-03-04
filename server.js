2927113b4b82a948c1dbf5a25f97ff7e3a043926
geceleriesen <etufan@gmail.com>
2026-03-04 21:36:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from "cors"
 import dotenv from "dotenv"
 
 import {getAllVideos} from "./youtube.js"
-import {detectLocation} from "./locationDetector.js"
+import {detectLocation} from "./locationEngine.js"
 
 dotenv.config()
 
@@ -16,7 +16,7 @@ let cache = []
 
 async function buildMap(){
 
-console.log("Scanning YouTube channel...")
+console.log("Scanning channel...")
 
 const videos = await getAllVideos()
 
@@ -31,9 +31,8 @@ if(!location) continue
 let lat = location.lat
 let lng = location.lng
 
-// small offset to avoid overlapping pins
-lat += (Math.random()-0.5)*0.5
-lng += (Math.random()-0.5)*0.5
+lat += (Math.random()-0.5)*0.3
+lng += (Math.random()-0.5)*0.3
 
 results.push({
 
@@ -50,7 +49,7 @@ thumbnail:`https://img.youtube.com/vi/${v.id}/hqdefault.jpg`
 
 cache = results
 
-console.log("Videos mapped:", results.length)
+console.log("Videos mapped:",results.length)
 
 }
 
@@ -60,7 +59,7 @@ res.json(cache)
 
 })
 
-app.listen(PORT, async ()=>{
+app.listen(PORT,async()=>{
 
 console.log("Travel Map Engine running")
 
