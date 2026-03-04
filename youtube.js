00416389006b3d86e21c22cd40ab49b34898c6ee
geceleriesen <etufan@gmail.com>
2026-03-04 21:36:54 +0300

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,34 +1,34 @@
 import fetch from "node-fetch"
 
-const API = "https://www.googleapis.com/youtube/v3"
+const API="https://www.googleapis.com/youtube/v3"
 
-const KEY = process.env.YOUTUBE_API_KEY
-const CHANNEL = process.env.YOUTUBE_CHANNEL_ID
+const KEY=process.env.YOUTUBE_API_KEY
+const CHANNEL=process.env.YOUTUBE_CHANNEL_ID
+
 
 async function getUploadsPlaylist(){
 
-const url =
-`${API}/channels?part=contentDetails&id=${CHANNEL}&key=${KEY}`
+const url=`${API}/channels?part=contentDetails&id=${CHANNEL}&key=${KEY}`
 
-const res = await fetch(url)
-const data = await res.json()
+const res=await fetch(url)
+const data=await res.json()
 
 return data.items[0].contentDetails.relatedPlaylists.uploads
 
 }
 
+
 async function getPlaylistVideos(playlist){
 
-let pageToken = ""
-const videos = []
+let pageToken=""
+const videos=[]
 
 do{
 
-const url =
-`${API}/playlistItems?part=snippet&maxResults=50&playlistId=${playlist}&pageToken=${pageToken}&key=${KEY}`
+const url=`${API}/playlistItems?part=snippet&maxResults=50&playlistId=${playlist}&pageToken=${pageToken}&key=${KEY}`
 
-const res = await fetch(url)
-const data = await res.json()
+const res=await fetch(url)
+const data=await res.json()
 
 data.items.forEach(v=>{
 
@@ -42,7 +42,7 @@ description:v.snippet.description
 
 })
 
-pageToken = data.nextPageToken
+pageToken=data.nextPageToken
 
 }while(pageToken)
 
@@ -50,9 +50,10 @@ return videos
 
 }
 
+
 export async function getAllVideos(){
 
-const uploads = await getUploadsPlaylist()
+const uploads=await getUploadsPlaylist()
 return await getPlaylistVideos(uploads)
 
 }
