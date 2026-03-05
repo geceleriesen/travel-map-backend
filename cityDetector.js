e6180544c8df0268232f5f18e141fe1c13641fdc
geceleriesen <etufan@gmail.com>
2026-03-05 19:56:11 +0300

    Update cityDetector.js

--- cityDetector.js
+++ cityDetector.js
@@ -9,14 +9,12 @@ for(let i=1;i<lines.length;i++){
 
 const p = lines[i].split(",")
 
-if(p.length<4) continue
+if(p.length < 4) continue
 
 cities.push({
-
 name:p[0].toLowerCase(),
 lat:parseFloat(p[2]),
 lng:parseFloat(p[3])
-
 })
 
 }
@@ -28,9 +26,7 @@ title = title.toLowerCase()
 for(const city of cities){
 
 if(title.includes(city.name)){
-
 return city
-
 }
 
 }
