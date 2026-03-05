1053aa03e7e1116faa28f3468e081cd95e5072c2
geceleriesen <etufan@gmail.com>
2026-03-05 06:06:31 +0300

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,47 +1,56 @@
 import fetch from "node-fetch"
 
-export async function getChannelVideos(channel){
+const API="https://www.googleapis.com/youtube/v3"
 
-const url = `https://www.youtube.com/${channel}/videos`
+const API_KEY=process.env.YOUTUBE_API_KEY
+const CHANNEL_ID=process.env.YOUTUBE_CHANNEL_ID
 
-const html = await fetch(url).then(r=>r.text())
 
-if(!html.includes("ytInitialData")) return []
 
-const json = html.split("var ytInitialData =")[1].split(";</script>")[0]
+async function getUploadsPlaylist(){
 
-const data = JSON.parse(json)
+const url =
+`${API}/channels?part=contentDetails&id=${CHANNEL_ID}&key=${API_KEY}`
 
-const tabs =
-data.contents.twoColumnBrowseResultsRenderer.tabs
+const res=await fetch(url)
+const data=await res.json()
 
+return data.items[0].contentDetails.relatedPlaylists.uploads
+
+}
+
+
+
+export async function getAllVideos(){
+
+const playlist=await getUploadsPlaylist()
+
+let pageToken=""
 let videos=[]
 
-for(const tab of tabs){
+do{
 
-const content =
-tab.tabRenderer?.content?.richGridRenderer?.contents
+const url =
+`${API}/playlistItems?part=snippet&maxResults=50&playlistId=${playlist}&pageToken=${pageToken}&key=${API_KEY}`
 
-if(!content) continue
+const res=await fetch(url)
+const data=await res.json()
 
-for(const item of content){
-
-const video =
-item.richItemRenderer?.content?.videoRenderer
-
-if(!video) continue
+for(const item of data.items){
 
 videos.push({
 
-id:video.videoId,
-title:video.title.runs[0].text,
-thumbnail:video.thumbnail.thumbnails.pop().url
+id:item.snippet.resourceId.videoId,
+title:item.snippet.title,
+thumbnail:item.snippet.thumbnails.high.url
 
 })
 
 }
 
-}
+pageToken=data.nextPageToken
+
+}while(pageToken)
 
 return videos
 
