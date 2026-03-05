c161ebac66b9dc179cbc827823ac93d2d75184a5
geceleriesen <etufan@gmail.com>
2026-03-05 07:09:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,45 +1,27 @@
 import express from "express"
 import cors from "cors"
-import Parser from "rss-parser"
+import getVideos from "./rss.js"
 
 const app = express()
-const parser = new Parser()
 
 app.use(cors())
 
-const CHANNEL_ID = process.env.YOUTUBE_CHANNEL_ID
-
-app.get("/", (req,res)=>{
-res.send("Backend running")
+app.get("/",(req,res)=>{
+res.send("Travel map backend running")
 })
 
-app.get("/api/videos", async (req,res)=>{
+app.get("/api/videos", async(req,res)=>{
 
 try{
 
-const feed = await parser.parseURL(
-`https://www.youtube.com/feeds/videos.xml?channel_id=${CHANNEL_ID}`
-)
-
-const videos = feed.items.map(v=>{
-
-const id=v.id.split(":").pop()
-
-return{
-id:id,
-title:v.title,
-thumbnail:`https://img.youtube.com/vi/${id}/hqdefault.jpg`,
-lat:20+(Math.random()*40-20),
-lng:0+(Math.random()*60-30)
-}
-
-})
+const videos = await getVideos()
 
 res.json(videos)
 
 }catch(e){
 
 console.log(e)
+
 res.json([])
 
 }
@@ -50,6 +32,6 @@ const PORT = process.env.PORT || 3000
 
 app.listen(PORT,()=>{
 
-console.log("Server running on",PORT)
+console.log("server running",PORT)
 
 })
