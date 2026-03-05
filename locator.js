eabe6f95f42ffd8cad776ed757d41737fd3c94d9
geceleriesen <etufan@gmail.com>
2026-03-05 18:41:15 +0300

    Update locator.js

--- locator.js
+++ locator.js
@@ -1,34 +1,8 @@
 export default async function locate(title){
 
-title = title.toLowerCase()
-
-const words = title.split(" ")
-
-for(const word of words){
-
-if(word.length < 4) continue
-
-try{
-
-const res = await fetch(
-`https://nominatim.openstreetmap.org/search?q=${word}&format=json&limit=1`
-)
-
-const data = await res.json()
-
-if(data.length){
-
 return {
-lat: parseFloat(data[0].lat),
-lng: parseFloat(data[0].lon)
+lat:0,
+lng:0
 }
-
-}
-
-}catch(e){}
-
-}
-
-return {lat:0,lng:0}
 
 }
