6d91c71bae1c1281b1977955d618a9f522debcbc
geceleriesen <etufan@gmail.com>
2026-03-05 05:03:39 +0300

    Update cityDetector.js

--- cityDetector.js
+++ cityDetector.js
@@ -1,1 +1,25 @@
+import fs from "fs"
 
+const cities =
+JSON.parse(fs.readFileSync("./cities.json"))
+
+export function detectCity(title){
+
+const t = title.toLowerCase()
+
+for(const c of cities){
+
+if(t.includes(c.name.toLowerCase())){
+
+return {
+lat:c.lat,
+lng:c.lng
+}
+
+}
+
+}
+
+return null
+
+}
