7cb953644333754efa9e5ffd2ffce08991169750
geceleriesen <etufan@gmail.com>
2026-03-05 07:01:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,45 @@
 import express from "express"
 import cors from "cors"
-import getVideos from "./rss.js"
+import Parser from "rss-parser"
 
 const app = express()
+const parser = new Parser()
 
 app.use(cors())
 
+const CHANNEL_ID = process.env.YOUTUBE_CHANNEL_ID
+
 app.get("/", (req,res)=>{
-res.send("Travel Map Backend Running")
+res.send("Backend running")
 })
 
 app.get("/api/videos", async (req,res)=>{
 
 try{
 
-const videos = await getVideos()
+const feed = await parser.parseURL(
+`https://www.youtube.com/feeds/videos.xml?channel_id=${CHANNEL_ID}`
+)
+
+const videos = feed.items.map(v=>{
+
+const id=v.id.split(":").pop()
+
+return{
+id:id,
+title:v.title,
+thumbnail:`https://img.youtube.com/vi/${id}/hqdefault.jpg`,
+lat:20+(Math.random()*40-20),
+lng:0+(Math.random()*60-30)
+}
+
+})
 
 res.json(videos)
 
 }catch(e){
 
 console.log(e)
-
 res.json([])
 
 }
@@ -31,5 +49,7 @@ res.json([])
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT,()=>{
-console.log("Server running",PORT)
+
+console.log("Server running on",PORT)
+
 })
