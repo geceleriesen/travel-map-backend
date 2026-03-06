eaaa61ebd8ecd02ff76e61023038b18cdfd1358e
geceleriesen <etufan@gmail.com>
2026-03-06 20:04:27 +0300

    Update countries.js

--- countries.js
+++ countries.js
@@ -1,21 +1,21 @@
 export const countries = {
 
-"meksika":{name:"Meksika",lat:23.6,lng:-102.5},
+"meksika":{name:"Mexico",lat:23.6,lng:-102.5},
 "mexico":{name:"Mexico",lat:23.6,lng:-102.5},
 
-"mısır":{name:"Mısır",lat:26.8,lng:30.8},
+"mısır":{name:"Egypt",lat:26.8,lng:30.8},
 "egypt":{name:"Egypt",lat:26.8,lng:30.8},
 
-"hindistan":{name:"Hindistan",lat:22.3,lng:78.9},
+"hindistan":{name:"India",lat:22.3,lng:78.9},
 "india":{name:"India",lat:22.3,lng:78.9},
 
-"malezya":{name:"Malezya",lat:4.2,lng:102.0},
+"malezya":{name:"Malaysia",lat:4.2,lng:102.0},
 "malaysia":{name:"Malaysia",lat:4.2,lng:102.0},
 
-"porto riko":{name:"Porto Riko",lat:18.22,lng:-66.59},
+"porto riko":{name:"Puerto Rico",lat:18.22,lng:-66.59},
 "puerto rico":{name:"Puerto Rico",lat:18.22,lng:-66.59},
 
-"japonya":{name:"Japonya",lat:36.2,lng:138.2},
+"japonya":{name:"Japan",lat:36.2,lng:138.2},
 "japan":{name:"Japan",lat:36.2,lng:138.2}
 
 }
