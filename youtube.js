86e2c9690730a39ced1885e085cfb980018117f0
geceleriesen <etufan@gmail.com>
2026-03-06 18:27:40 +0300

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,37 +1,40 @@
 import detectCity from "./cityDetector.js"
 
 const API_KEY = process.env.YOUTUBE_API_KEY
-
 const CHANNEL_ID = "UC0p5p9C6TQ6J9o7y1yZ0B2A"
+
+async function getUploadsPlaylist(){
+
+const url=`https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=${CHANNEL_ID}&key=${API_KEY}`
+
+const res = await fetch(url)
+const data = await res.json()
+
+return data.items[0].contentDetails.relatedPlaylists.uploads
+}
 
 export default async function getVideos(){
 
-if(!API_KEY){
-console.log("NO API KEY")
-return []
-}
+const playlistId = await getUploadsPlaylist()
 
 let videos=[]
 let nextPage=""
 
 while(true){
 
-const url=`https://www.googleapis.com/youtube/v3/search?part=snippet,id&type=video&channelId=${CHANNEL_ID}&maxResults=50&key=${API_KEY}&pageToken=${nextPage}`
+const url=`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&maxResults=50&key=${API_KEY}&pageToken=${nextPage}`
 
 const res = await fetch(url)
-
 const data = await res.json()
-
-console.log("YT RESPONSE:",data)
 
 if(!data.items) break
 
 for(const item of data.items){
 
-const id=item.id.videoId
-const title=item.snippet.title
+const id = item.snippet.resourceId.videoId
+const title = item.snippet.title
 
-const city=detectCity(title)
+const city = detectCity(title)
 
 videos.push({
 id,
@@ -48,6 +51,8 @@ if(!data.nextPageToken) break
 nextPage=data.nextPageToken
 
 }
+
+console.log("VIDEOS FOUND:",videos.length)
 
 return videos
 
