ae743a397a9ebdc66f8bead4c4108eb1ae6d2d05
geceleriesen <etufan@gmail.com>
2026-03-06 18:21:28 +0300

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -6,41 +6,46 @@ const CHANNEL_ID = "UC0p5p9C6TQ6J9o7y1yZ0B2A"
 
 export default async function getVideos(){
 
+if(!API_KEY){
+console.log("NO API KEY")
+return []
+}
+
 let videos=[]
 let nextPage=""
 
 while(true){
 
-const url=`https://www.googleapis.com/youtube/v3/search?part=snippet,id&channelId=${CHANNEL_ID}&maxResults=50&order=date&type=video&key=${API_KEY}&pageToken=${nextPage}`
+const url=`https://www.googleapis.com/youtube/v3/search?part=snippet,id&type=video&channelId=${CHANNEL_ID}&maxResults=50&key=${API_KEY}&pageToken=${nextPage}`
 
 const res = await fetch(url)
 
 const data = await res.json()
 
+console.log("YT RESPONSE:",data)
+
 if(!data.items) break
 
 for(const item of data.items){
 
-const id = item.id.videoId
-const title = item.snippet.title
+const id=item.id.videoId
+const title=item.snippet.title
 
-const city = detectCity(title)
+const city=detectCity(title)
 
 videos.push({
-
 id,
 title,
 thumbnail:`https://img.youtube.com/vi/${id}/hqdefault.jpg`,
 lat:city.lat,
 lng:city.lng
-
 })
 
 }
 
 if(!data.nextPageToken) break
 
-nextPage = data.nextPageToken
+nextPage=data.nextPageToken
 
 }
 
