06be6358b273bac4fec174bfe8563ed859e32ca6
geceleriesen <etufan@gmail.com>
2026-03-06 19:37:50 +0300

    Create countries.js

---
+++ countries.js
@@ -0,0 +1,14 @@
+export const countries = {
+
+turkiye:{lat:39.0,lng:35.0},
+mexico:{lat:23.6,lng:-102.5},
+egypt:{lat:26.8,lng:30.8},
+india:{lat:22.3,lng:78.9},
+malaysia:{lat:4.2,lng:102.0},
+puerto:{lat:18.2,lng:-66.5},
+brazil:{lat:-14.2,lng:-51.9},
+thailand:{lat:15.8,lng:100.9},
+japan:{lat:36.2,lng:138.2},
+china:{lat:35.8,lng:104.1}
+
+}
