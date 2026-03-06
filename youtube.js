2a178f333727c36b56f142dea83f0fb0e77177c3
geceleriesen <etufan@gmail.com>
2026-03-06 18:45:51 +0300

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,16 +1,27 @@
 import detectCity from "./cityDetector.js"
 
 const API_KEY = process.env.YOUTUBE_API_KEY
-const CHANNEL_ID = "UC0p5p9C6TQ6J9o7y1yZ0B2A"
+
+const CHANNEL_ID = "UCvVqEXXv4y0FZ7X4YhY9F7Q"
 
 async function getUploadsPlaylist(){
 
 const url=`https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=${CHANNEL_ID}&key=${API_KEY}`
 
 const res = await fetch(url)
+
 const data = await res.json()
 
+console.log("CHANNEL RESPONSE:",data)
+
+if(!data.items || data.items.length===0){
+
+throw new Error("Channel not found")
+
+}
+
 return data.items[0].contentDetails.relatedPlaylists.uploads
+
 }
 
 export default async function getVideos(){
@@ -25,7 +36,10 @@ while(true){
 const url=`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&maxResults=50&key=${API_KEY}&pageToken=${nextPage}`
 
 const res = await fetch(url)
+
 const data = await res.json()
+
+console.log("PLAYLIST RESPONSE:",data)
 
 if(!data.items) break
 
@@ -37,11 +51,13 @@ const title = item.snippet.title
 const city = detectCity(title)
 
 videos.push({
+
 id,
 title,
 thumbnail:`https://img.youtube.com/vi/${id}/hqdefault.jpg`,
 lat:city.lat,
 lng:city.lng
+
 })
 
 }
