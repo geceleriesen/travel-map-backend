b8c24f471731c94d84206b4aa1f204b7cf30d83f
geceleriesen <etufan@gmail.com>
2026-03-06 20:10:45 +0300

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -5,55 +5,77 @@ const CHANNEL_ID = "UCHut-IQXip7mtXyC3GOiQ1A"
 
 async function getUploadsPlaylist(){
 
-const url = `https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=${CHANNEL_ID}&key=${API_KEY}`
+const url =
+"https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id="
++ CHANNEL_ID +
+"&key=" + API_KEY
 
 const res = await fetch(url)
+
 const data = await res.json()
 
+if(!data.items || data.items.length === 0){
+
+console.log("Channel not found")
+
+return null
+
+}
+
 return data.items[0].contentDetails.relatedPlaylists.uploads
+
 }
 
 export default async function getVideos(){
 
 const playlistId = await getUploadsPlaylist()
 
+if(!playlistId){
+
+return []
+
+}
+
 let videos=[]
 let nextPage=""
 
 while(true){
 
-const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&maxResults=50&key=${API_KEY}&pageToken=${nextPage}`
+const url =
+"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet"
++"&playlistId="+playlistId
++"&maxResults=50"
++"&key="+API_KEY
++"&pageToken="+nextPage
 
 const res = await fetch(url)
+
 const data = await res.json()
 
 if(!data.items) break
 
 for(const item of data.items){
 
+if(!item.snippet) continue
+
 const id = item.snippet.resourceId.videoId
+
 const title = item.snippet.title
 
 const city = detectCity(title)
 
-const city = detectCity(title)
-
-videos.push({
-id,
-title,
-thumbnail:`https://img.youtube.com/vi/${id}/hqdefault.jpg`,
-lat:city.lat,
-lng:city.lng,
-location:city.name
-})
-
 videos.push({
 
-id,
-title,
-thumbnail:`https://img.youtube.com/vi/${id}/hqdefault.jpg`,
+id:id,
+
+title:title,
+
+thumbnail:"https://img.youtube.com/vi/"+id+"/hqdefault.jpg",
+
 lat:city.lat,
+
 lng:city.lng,
+
 location:city.name
 
 })
@@ -62,9 +84,11 @@ location:city.name
 
 if(!data.nextPageToken) break
 
-nextPage=data.nextPageToken
+nextPage = data.nextPageToken
 
 }
+
+console.log("VIDEOS FOUND:",videos.length)
 
 return videos
 
