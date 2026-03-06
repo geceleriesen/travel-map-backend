7070fe9f29834a58e48dfb769bce7fbd63b40d71
geceleriesen <etufan@gmail.com>
2026-03-06 21:29:18 +0300

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,74 +1,32 @@
-import locationAI from "./locationAI.js"
+const { resolveLocation } = require("./locationAI");
 
-const API_KEY = process.env.YOUTUBE_API_KEY
-const CHANNEL_ID = "UCHut-IQXip7mtXyC3GOiQ1A"
+async function processVideos(videos) {
 
-async function getUploadsPlaylist(){
+  return videos.map((video) => {
 
-const url =
-"https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id="
-+ CHANNEL_ID +
-"&key=" + API_KEY
+    const loc = resolveLocation(video);
 
-const res = await fetch(url)
-const data = await res.json()
+    if (!loc) {
 
-return data.items[0].contentDetails.relatedPlaylists.uploads
-}
+      video.location = "Unknown";
+      video.lat = null;
+      video.lng = null;
 
-export default async function getVideos(){
+      return video;
 
-const playlistId = await getUploadsPlaylist()
+    }
 
-let videos=[]
-let nextPage=""
+    video.location = loc.location;
+    video.country = loc.country;
+    video.lat = loc.lat;
+    video.lng = loc.lng;
 
-while(true){
+    return video;
 
-const url =
-"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet"
-+"&playlistId="+playlistId
-+"&maxResults=50"
-+"&key="+API_KEY
-+"&pageToken="+nextPage
-
-const res = await fetch(url)
-const data = await res.json()
-
-if(!data.items) break
-
-for(const item of data.items){
-
-const id = item.snippet.resourceId.videoId
-const title = item.snippet.title
-
-const loc = locationAI(title)
-
-// eğer AI bulamazsa bile boş obje dönmesin
-
-videos.push({
-
-id:id,
-
-title:title,
-
-thumbnail:"https://img.youtube.com/vi/"+id+"/hqdefault.jpg",
-
-lat:loc?.lat ?? null,
-lng:loc?.lng ?? null,
-location:loc?.name ?? "Unknown"
-
-})
+  });
 
 }
 
-if(!data.nextPageToken) break
-nextPage=data.nextPageToken
-
-}
-
-console.log("VIDEOS FOUND:",videos.length)
-
-return videos
-
-}
+module.exports = {
+  processVideos
+};
