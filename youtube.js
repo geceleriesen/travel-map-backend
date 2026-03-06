46cb2e68f900f2c2cb5b75f97614150fc2cdfaa5
geceleriesen <etufan@gmail.com>
2026-03-06 17:35:07 +0300

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,33 +1,44 @@
-import Parser from "rss-parser"
 import detectCity from "./cityDetector.js"
 
-const parser = new Parser()
-
+const API_KEY = process.env.YOUTUBE_API_KEY
 const CHANNEL_ID = "UC6v0Y9lP3Lq6V0Kk9d0PZ1Q"
 
 export default async function getVideos(){
 
-const url=`https://www.youtube.com/feeds/videos.xml?channel_id=${CHANNEL_ID}`
+let videos=[]
+let nextPage=""
 
-const feed = await parser.parseURL(url)
+while(true){
 
-const videos=[]
+const url=`https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=50&pageToken=${nextPage}`
 
-for(const item of feed.items){
+const res=await fetch(url)
+const data=await res.json()
 
-const id=item.id.split(":").pop()
+for(const item of data.items){
 
-const city=detectCity(item.title)
+if(item.id.kind!=="youtube#video") continue
+
+const id=item.id.videoId
+const title=item.snippet.title
+
+const city=detectCity(title)
 
 videos.push({
 
 id,
-title:item.title,
+title,
 thumbnail:`https://img.youtube.com/vi/${id}/hqdefault.jpg`,
 lat:city.lat,
 lng:city.lng
 
 })
+
+}
+
+if(!data.nextPageToken) break
+
+nextPage=data.nextPageToken
 
 }
 
