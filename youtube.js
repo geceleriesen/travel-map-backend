844fd7d190242a669cf617ddd347e424a5bf5fd6
geceleriesen <etufan@gmail.com>
2026-03-06 22:44:07 +0300

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,15 +1,50 @@
-const {resolveLocation}=require("./locationAI");
+const fetch = require("node-fetch");
+const { resolveLocation } = require("./locationAI");
 
-function processVideos(videos){
+const API_KEY = process.env.YOUTUBE_API_KEY;
 
-return videos.map(v=>{
+async function fetchTravelVideos(){
 
-const loc=resolveLocation(v);
+const url =
+`https://www.googleapis.com/youtube/v3/search
+?part=snippet
+&q=travel vlog
+&type=video
+&maxResults=50
+&key=${API_KEY}`;
+
+const res = await fetch(url);
+
+const data = await res.json();
+
+const videos = data.items.map(v=>({
+
+id:v.id.videoId,
+
+title:v.snippet.title,
+
+description:v.snippet.description,
+
+thumbnail:v.snippet.thumbnails.high.url
+
+}));
+
+return videos;
+
+}
+
+
+async function getVideos(){
+
+const rawVideos = await fetchTravelVideos();
+
+const processed = rawVideos.map(v=>{
+
+const loc = resolveLocation(v);
 
 if(!loc){
 
 v.location="Unknown";
-v.country="Unknown";
 v.lat=null;
 v.lng=null;
 v.locationType="unknown";
@@ -18,16 +53,18 @@ return v;
 
 }
 
-v.location=loc.location;
-v.country=loc.country;
-v.lat=loc.lat;
-v.lng=loc.lng;
-v.locationType=loc.type;
+v.location = loc.location;
+v.country = loc.country;
+v.lat = loc.lat;
+v.lng = loc.lng;
+v.locationType = loc.type;
 
 return v;
 
 });
 
+return processed;
+
 }
 
-module.exports={processVideos};
+module.exports = { getVideos };
